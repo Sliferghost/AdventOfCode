1f56a1fe29a0784053d30af5598715ca96c056bd
Jaap Oudejans <jaapoudejans@gmail.com>
2020-12-01 17:28:30 +0100

    Added a scenario for having multiple expenses in the report

--- 2020/Day-1/test_solution.py
+++ 2020/Day-1/test_solution.py
@@ -5,6 +5,10 @@ def test_findExpenses_single_expense():
     expenses = findExpenses(expenseReport, 2)
     assert expenses == 1
 
+def test_findExpenses_multiple_expenses():
+    expenseReport = [1,2,3,4,5]
+    expenses = findExpenses(expenseReport, 6)
+    assert expenses == 5
 
 
 # expenses = []
